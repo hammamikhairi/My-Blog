62fa6ea2e255aecc239608d9981b0b10802c5e0e
HammamiKhairi <hammamikhairi@gmail.com>
2022-06-05 20:20:49 +0100

    added motion to blogs list

--- src/components/Blogs/Blogs-card.jsx
+++ src/components/Blogs/Blogs-card.jsx
@@ -1,22 +1,27 @@
+import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
 import "./blogs.sass";
 
 const Blogs = ({blogs}) => {
   return (
-    <div className="blogs">
-
-      {blogs && blogs.map( blog =>
-        <div className="blog-preview"  key={blog.id}>
-          <Link to={`/myblog/blog/${blog.id}`} >
-            <div className="blog-img" />
-            <h4>{blog.date}</h4>
-            <h2>{blog.title}</h2>
-            <p>{blog.description} </p>
-            <h3>Read More...</h3>
-          </Link>
-        </div>
-      )}
-    </div>
+    <div  className="blogs">
+        {blogs && blogs.map( blog =>
+          <motion.div
+          initial={{y:'-10vh', opacity: 0}}
+          animate={{y:0, opacity: 1}}
+          transition={{type:'spring', duration:1.5, delay:0.5 * blog.id}}
+          className="blog-preview"
+          key={blog.id}
+          >
+            <Link to={`/myblog/blog/${blog.id}`} >
+              {/* <div className="blog-img" style={{backgroundImage: `url(${blog.banner})`}} /> */}
+              <h2>{blog.title}</h2>
+              <p>{blog.description} </p>
+              <h4>{blog.date} · {blog.long}</h4>
+            </Link>
+          </motion.div>
+        )}
+      </div>
   );
 }
 
