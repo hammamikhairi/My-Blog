5eb949b350c6cf08de29daf129699e322c2219f2
HammamiKhairi <hammamikhairi@gmail.com>
2022-06-05 20:19:52 +0100

    added dark mode

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,19 @@
 import { Link } from "react-router-dom";
 import "./navbar.sass";
+import { DarkModeSwitch } from 'react-toggle-dark-mode';
+import { useState } from "react";
 
 const Navbar = () => {
+
+  const [isDarkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!isDarkMode);
+    console.log("dark :", isDarkMode);
+    let bod = document.getElementsByTagName("body")[0];
+    bod.classList.toggle("light-body")
+    bod.classList.toggle("dark-body")
+  }
 
 
 
@@ -13,7 +25,12 @@ const Navbar = () => {
         </div>
         <ul className="links">
           <Link to="/myblog/cv"><li className="link">CV</li></Link>
-          <Link to="/myblog/about"><li className="link">ABOUT ME</li></Link>
+          <DarkModeSwitch
+            style={{ marginBottom: '0px' }}
+            checked={isDarkMode}
+            onChange={toggleDarkMode}
+            size={25}
+          />
         </ul>
       </div>
     </div>
