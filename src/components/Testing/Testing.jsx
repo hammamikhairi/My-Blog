6f6b86dcb6db0f8ee48c279a8a17b56571359d22
HammamiKhairi <hammamikhairi@gmail.com>
2022-07-22 22:35:20 +0100

    Removed testing files

--- src/components/Testing/Testing.jsx
+++
@@ -1,9 +0,0 @@
-import Markdown from 'markdown-to-jsx';
-
-
-
-const Testing = () => {
-  return <Markdown># Hello world!</Markdown>
-}
- 
-export default Testing;
--- src/components/Testing/hell.md
+++
@@ -1,1 +0,0 @@
-[hell](www.google.com)
