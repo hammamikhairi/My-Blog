c533059a94ee129e6f1d512fca9af956f890a9b7
HammamiKhairi <hammamikhairi@gmail.com>
2022-07-24 01:35:57 +0100

    Minor Fixes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,12 +3,22 @@ import headerIllustration from "../../images/1-01.png";
 import "./header.sass";
 
 const Header = () => {
+  let watcher = window.innerWidth <= 970;
+  console.log(watcher);
   return (
       <div className="header-container"
       >
         <motion.div
-        initial={{x:'-10vw', opacity: 0}}
-        animate={{x:0, opacity: 1}}
+        initial={{
+          x: watcher ? 0:  '-10vw',
+          y: watcher ?'-10vh' :  0,
+          opacity: 0
+        }}
+        animate={{
+          x:0,
+          y:0,
+          opacity: 1
+        }}
         transition={{type:'spring', duration:1.5}} className="header-description"
         >
           <h1>A Blog!</h1>
@@ -16,8 +26,16 @@ const Header = () => {
           <p>Just another student trying to make sense out of his life.</p>
         </motion.div>
         <motion.div className="illustration-container"
-        initial={{x:'10vw', opacity: 0}}
-        animate={{x:0, opacity: 1}}
+        initial={{
+          x: watcher ? 0:  '-10vw',
+          y: watcher ?'-10vh' :  0,
+          opacity: 0
+        }}
+        animate={{
+          x:0,
+          y:0,
+          opacity: 1
+        }}
         transition={{type:'spring', duration:1.5}}>
           <img src={headerIllustration} alt="logo" className="illustration" />
         </motion.div>
