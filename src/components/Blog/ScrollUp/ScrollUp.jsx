26b47260f0d832d956b680a680f1cc8f56f8af02
HammamiKhairi <hammamikhairi@gmail.com>
2022-07-30 22:25:29 +0100

    scroll up feature ( not included yet )

---
+++ src/components/Blog/ScrollUp/ScrollUp.jsx
@@ -0,0 +1,29 @@
+import { motion } from 'framer-motion';
+import { AiOutlineUp } from 'react-icons/ai';
+import './scrollUp.sass';
+
+export const ScrollUp = () => {
+  console.log('scroll up');
+  return (
+    <>
+    {window.innerWidth > 768 &&
+    <motion.div
+      initial={{ opacity: 0}}
+      animate={{ opacity: 1}}
+      transition={{
+        duration:1,
+      }}
+      whileHover={{
+        scale: 1.5,
+        transition: { duration:  1 },
+      }}
+
+      onClick={() => window.scrollTo(0, 0)}
+      className="scroll__container"
+      >
+          <AiOutlineUp />
+      </motion.div>
+    }
+    </>
+  )
+}
